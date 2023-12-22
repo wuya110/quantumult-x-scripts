624222a0988caa774f6e96e9749bc8280cb1450c
wuya110 <147088502+wuya110@users.noreply.github.com>
2023-12-22 23:39:31 +0800

    Update get_cookie.js

--- get_cookie.js
+++ get_cookie.js
@@ -1,6 +1,6 @@
 if ($request.headers['Cookie']) {
     var cookie = $request.headers['Cookie'];
-    $prefs.setValueForKey(cookie, 'qmkg_cookie');
+    $prefs.setValueForKey(cookie, 'pduoduo_cookie');
     $notify("获取 Cookie 成功", "", "Cookie: " + cookie);
 } else {
     $notify("获取 Cookie 失败", "", "请确保你已经登录");
