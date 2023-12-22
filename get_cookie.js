e9801f630fb053129a6345142b51934468931370
wuya110 <147088502+wuya110@users.noreply.github.com>
2023-12-22 20:54:29 +0800

    Add files via upload

---
+++ get_cookie.js
@@ -0,0 +1,8 @@
+if ($request.headers['Cookie']) {
+    var cookie = $request.headers['Cookie'];
+    $prefs.setValueForKey(cookie, 'v2free_cookie');
+    $notify("获取 Cookie 成功", "", "Cookie: " + cookie);
+} else {
+    $notify("获取 Cookie 失败", "", "请确保你已经登录");
+}
+$done();
