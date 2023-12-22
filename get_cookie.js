2ad577de104e04ee0e1c3360403cb689eafb9156
wuya110 <147088502+wuya110@users.noreply.github.com>
2023-12-23 01:21:26 +0800

    更新 get_cookie.js

--- get_cookie.js
+++ get_cookie.js
@@ -1,8 +1,14 @@
-if ($request.headers['Cookie']) {
-    var cookie = $request.headers['Cookie'];
-    $prefs.setValueForKey(cookie, 'pduoduo_cookie');
-    $notify("获取 Cookie 成功", "", "Cookie: " + cookie);
-} else {
-    $notify("获取 Cookie 失败", "", "请确保你已经登录");
-}
-$done();
+var counter = 0;
+var cookieInterval = setInterval(function() {
+    if ($request.headers['Cookie']) {
+        var cookie = $request.headers['Cookie'];
+        $prefs.setValueForKey(cookie, 'pduoduo_cookie');
+        $notify("获取 Cookie 成功", "", "Cookie: " + cookie);
+    } else {
+        $notify("获取 Cookie 失败", "", "请确保你已经登录");
+    }
+    counter += 5;
+    if (counter >= 15) {
+        clearInterval(cookieInterval);
+    }
+}, 5000);
