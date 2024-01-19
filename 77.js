bbae58ee6523ee83f51c577ad6dfaff94729245e
wuya110 <147088502+wuya110@users.noreply.github.com>
2024-01-20 00:04:40 +0800

    更新 77.js

---
+++ 77.js
@@ -0,0 +1,24 @@
+[MITM]
+hostname = node.kg.qq.com
+[rewrite_local]
+# 获取全民K歌Cookie
+https:\/\/node\.kg\.qq\.com\/log\?format=json&outCharset=utf-8&g_tk url script-request-header https://raw.githubusercontent.com/wuya110/-001/main/6.js
+
+
+const cookieName = '全民K歌'
+const cookieKey = 'cookie_qmkg'
+const url = $request.url;
+
+if (url.indexOf("https://node.kg.qq.com/log?format=json&outCharset=utf-8&g_tk") >= 0) {
+  const cookieValue = $request.headers['Cookie'];
+  if (cookieValue) {
+    $prefs.setValueForKey(cookieValue, cookieKey);
+    $notify(cookieName, '获取Cookie成功', '');
+  } else {
+    $notify(cookieName, '获取Cookie失败', '无法从请求中提取Cookie');
+  }
+} else {
+    $notify(cookieName, '获取Cookie失败', 'URL不匹配');
+}
+
+$done({});
