f302e728424011b578bce6309d27c46fcf039826
wuya110 <147088502+wuya110@users.noreply.github.com>
2024-01-20 13:54:23 +0800

    更新 qmkg-ad.js

--- qmkg-ad.js
+++ qmkg-ad.js
@@ -5,12 +5,11 @@
 # 图标: https://gitlab.com/lodepuly/iconlibrary/-/raw/main/App_icon/120px/QQKSong.png
 # 日期: 2024-01-20 12:53:01
 
-[rewrite_remote]
 ^https:\/\/wallpaper-\d+\.file\.myqcloud\.com\/dsl\/ url 302 /reject-dict
 ^https?:\/\/wallpaper-\d+\.file\.myqcloud\.com\/hikari\/ url 302 /reject-dict
 ^https:\/\/amsweb-cdn-\S+-\d+\.file\.myqcloud\.com\/video\/ad_profile\/ url 302 /reject-dict
-^https:\/\/adsmind\.gdtimg\.com\/ads_svp_video.*\.(jpeg|jpg|png|webp) url reject-img
-^https:\/\/adsmind\.gdtimg\.com\/ads_svp_video.*mp4 url reject-video
+^https:\/\/adsmind\.gdtimg\.com\/ads_svp_video.*\.(jpeg|jpg|png|webp) reject-img
+^https:\/\/adsmind\.gdtimg\.com\/ads_svp_video.*mp4 reject-video
 
 [mitm]
 hostname = wallpaper-1258344696.file.myqcloud.com, amsweb-cdn-gz-1258344696.file.myqcloud.com, adsmind.gdtimg.com
