da40ae367f12192705dc27ea952800d2824f5459
wuya110 <147088502+wuya110@users.noreply.github.com>
2024-01-20 22:02:57 +0800

    更新 jichancookie.js

---
+++ jichancookie.js
@@ -0,0 +1,7 @@
+【Quantumult X】
+----------------
+[mitm]
+hostname = j01.tbcache.us
+
+[rewrite_local]
+https:\/\/j01\.tbcache\.us url script-request-header https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/glados/checkincookie_env.js
