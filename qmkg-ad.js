fe3f0569076b461950d5bbb9911a186499c6a35b
wuya110 <147088502+wuya110@users.noreply.github.com>
2024-01-20 13:46:11 +0800

    更新 qmkg-ad.js

--- qmkg-ad.js
+++ qmkg-ad.js
@@ -1,13 +1,21 @@
 # 全民K歌去广告
 # 移除开屏广告、歌房页面弹窗广告、消息页广告和我的页面广告。
-
+# 作者: 可莉🅥
+# 主页: https://gitlab.com/lodepuly/vpn_tool/-/tree/master/Tool/Loon/Plugin
+# 图标: https://gitlab.com/lodepuly/iconlibrary/-/raw/main/App_icon/120px/QQKSong.png
 # 日期: 2024-01-20 12:53:01
+
+[filter_local]
+// 添加你的本地拦截规则（如果有的话）
+
+[filter_remote]
+// 添加你的远程拦截规则（如果有的话）
 
 [rewrite_remote]
 ^https:\/\/wallpaper-\d+\.file\.myqcloud\.com\/dsl\/ url 302 /reject-dict
 ^https?:\/\/wallpaper-\d+\.file\.myqcloud\.com\/hikari\/ url 302 /reject-dict
 ^https:\/\/amsweb-cdn-\S+-\d+\.file\.myqcloud\.com\/video\/ad_profile\/ url 302 /reject-dict
-^https:\/\/adsmind\.gdtimg\.com\/ads_svp_video.*(jpeg|jpg|png|webp) url reject-img
+^https:\/\/adsmind\.gdtimg\.com\/ads_svp_video.*\.(jpeg|jpg|png|webp) url reject-img
 ^https:\/\/adsmind\.gdtimg\.com\/ads_svp_video.*mp4 url reject-video
 
 [mitm]
