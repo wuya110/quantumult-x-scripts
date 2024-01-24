e48a5553ee47148c554188f2180a42a4b6c75bb0
wuya110 <147088502+wuya110@users.noreply.github.com>
2024-01-25 03:22:37 +0800

    更新 77-1.js

--- 77-1.js
+++ 77-1.js
@@ -1,5 +1,4 @@
-[task_local]
-1 * * * * https://raw.githubusercontent.com/Keywos/rule/main/JS/surgeTroubleshoot.js, tag=ST Panel, update-interval=-1, enabled=true
+
 
 [rewrite_local]
 ^https?:\/\/(trouble\.shoot|surge\.tool) url script-request-header https://raw.githubusercontent.com/Keywos/rule/main/JS/surgeTroubleshoot.js
